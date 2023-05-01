accd71a22c9b97df65314f8380000c99243b4064
Gajanan Bogam <Gajanan.Bogam@cqc.org.uk>
2023-05-01 23:20:56 +0100

    adding missed cookieclicker_regressiontests

---
+++ CookieClicker_RegressionTests/StartPageTests.cs
@@ -0,0 +1,69 @@
+﻿
+using CookieClicker_Automation;
+using FluentAssertions;
+using Xunit;
+
+namespace CookieClicker_RegressionTests
+{
+    public class StartPageTests : TestBase
+    {
+        public StartPageTests()
+        {
+            LaunchCoockieClickerApplication();
+            StartTheGameByEnteringPlayerName();
+        }
+
+        [Fact]
+        public void VerifyTheSectionsOfStartPage()
+        {
+            // Verifyiong the Hello message at the Landing page.
+            startPage.WelcomeMessage.Displayed.Should().BeTrue();
+            // Verifying Cookies section is present with the default 0 value
+            startPage.Cookies.Displayed.Should().BeTrue();
+            startPage.Cookies.Text.Should().Be(Constants.Zero);
+            // Verifying Factories section is present woith default 0 value
+            startPage.Factories.Displayed.Should().BeTrue();
+            startPage.Factories.Text.Should().Be(Constants.Zero);
+            // Verifying Moneys section is present woith default 0 value
+            startPage.Money.Displayed.Should().BeTrue();
+            startPage.Money.Text.Should().Be(Constants.Zero);           
+            startPage.ClickCookieButton.Displayed.Should().BeTrue();
+            startPage.SellCookiesInputBox.Displayed.Should().BeTrue();
+            startPage.SellCookiesButton.Displayed.Should().BeTrue();
+            startPage.BuyFactoriesInputBox.Displayed.Should().BeTrue();
+            startPage.BuyFactoriesButton.Displayed.Should().BeTrue();
+            Dispose();
+        }
+
+        [Fact]
+        public void VerifySellCookiesFunctionality()
+        {
+            CollectCookies(5);
+            startPage.Cookies.Text.Should().Be("5");
+            SellCookies(Constants.SpecialCharacter);
+            startPage.Cookies.Text.Should().Be("5");
+            SellCookies(Constants.DecimalValue);
+            startPage.Cookies.Text.Should().Be("5");
+            SellCookies(Constants.Zero);
+            startPage.Cookies.Text.Should().Be("5");
+            SellCookies(Constants.NegativeValue);
+            startPage.Cookies.Text.Should().Be("5");
+            Dispose();
+        }
+
+        [Theory]
+        [InlineData(Constants.SpecialCharacter)]
+        [InlineData(Constants.DecimalValue)]
+        [InlineData(Constants.Zero)]
+        [InlineData(Constants.ValidNumber)]
+        public void VerifyBuyFactories(string value)
+        {
+            startPage.Cookies.Text.Should().Be(Constants.Zero);
+            startPage.Factories.Text.Should().Be(Constants.Zero);
+            startPage.Money.Text.Should().Be(Constants.Zero);
+            BuyFactories(value);
+            startPage.Factories.Text.Should().Be(Constants.Zero);
+            Dispose();
+        } 
+    }
+}
